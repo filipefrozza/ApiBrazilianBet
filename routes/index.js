9cde1116f845a99d3865b949c2080317312204f5
Filipe Frozza <filipefrozza@hotmail.com>
2019-07-24 13:51:53 -0300

    shelljs

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'BrazilianBet API' });
 });
 
+router.get('/webhook', function(res, res, nex){
+  var shell = require('shelljs');
+  var result = shell.exec('GIT_WORK_TREE=/home/ubuntu/node/ApiBrazilianBet git pull').code;
+  res.json({result: result==1?'success':'failed'});
+});
+
 module.exports = router;
