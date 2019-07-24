179fa4724f1beaed8fb727b76489a88b889cd508
Filipe Frozza <filipefrozza@hotmail.com>
2019-07-24 14:01:09 -0300

    shelljs webhook teste

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'BrazilianBet API' });
+  res.render('index', { title: 'API BrazilianBet' });
 });
 
 router.get('/webhook', function(res, res, nex){
