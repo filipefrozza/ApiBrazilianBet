2e6e1ad38ce6917f8554e51b59926adbce90ba93
Filipe Frozza <filipefrozza@hotmail.com>
2019-07-25 12:57:55 -0300

    faltou o post

--- routes/index.js
+++ routes/index.js
@@ -22,12 +22,11 @@ router.post('/webhook', function(res, res, nex){
   var shell = require('shelljs');
   require('shelljs/global');
   global.verbose = true;
-  var root = appRoot == "/root"?"/bb/node/ApiBrazilianBet":appRoot;
-  shell.cd(root);
+  shell.cd(appRoot.path);
   var result = "node - "+shell.exec('git pull');
   shell.cd('../../react/brazilian-bet');
   result += " | react - "+shell.exec('git pull');
-  result += " | "+root;
+  result += " | "+appRoot.path;
   res.json({result: result});
 });
 
